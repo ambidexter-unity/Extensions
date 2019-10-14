a7835167f4320c1fd648d86b46da8f8d5dc02d2b
GeorgAmisare <orlegand@gmail.com>
2019-10-14 13:20:23 +0300

    Доработка автодоводки скроллинга по секторам

--- Assets/test.cs
+++ Assets/test.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Extensions;
+using System.Linq;
 
 public class Test : MonoBehaviour
 {
     public Transform downLeftPoint;
     public Transform upRightPoint;
+
+    public List<GameObject> SectorCenters;
 
     private void Start()
     {
@@ -15,5 +18,7 @@ public class Test : MonoBehaviour
         Bounds bounds = new Bounds();
         bounds.SetMinMax(downLeftPoint.position, upRightPoint.position);
         cameraController.SetFocusObjectBounds(bounds);
+        List<Vector3> sectorCentersPositions = SectorCenters.Select(go => go.transform.position).ToList();
+        cameraController.SetSectorCenters(sectorCentersPositions);
     }
 }
