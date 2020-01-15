49a909d1e01ae2a3abc1dc8041e63146ce9317c5
aaamarkin <aaamarkin@gmail.com>
2020-01-15 16:41:27 +0300

    Added UnlockAll method

--- Assets/Plugins/Extensions/TouchHelper.cs
+++ Assets/Plugins/Extensions/TouchHelper.cs
@@ -1,9 +1,8 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Extensions.TouchHelperSubClasses;
-
 // ReSharper disable once CheckNamespace
 namespace Extensions
 {
@@ -13,19 +12,14 @@ namespace Extensions
     public static class TouchHelper
     {
         private static readonly TouchCreator LastFakeTouch = new TouchCreator();
-
         private static readonly TouchCreator ZoomGestureTouch1 = new TouchCreator();
         private static readonly TouchCreator ZoomGestureTouch2 = new TouchCreator();
-
         private static readonly Touch EmptyTouch = new Touch();
-
         private static int _lockerId;
         private static readonly List<int> Lockers = new List<int>();
-
 //#if !UNITY_EDITOR
 		private static readonly List<RaycastResult> Res = new List<RaycastResult>();
 //#endif
-
         /// <summary>
         /// Блокировка тачей.
         /// </summary>
@@ -36,7 +30,6 @@ namespace Extensions
             Lockers.Add(id);
             return id;
         }
-
         /// <summary>
         /// Разблокировка тачей.
         /// </summary>
@@ -45,12 +38,14 @@ namespace Extensions
         {
             Lockers.Remove(id);
         }
-
+        public static void UnlockAll()
+        {
+            Lockers.Clear();
+        }
         /// <summary>
         /// Флаг, указывающий на наличие блокировки тачей.
         /// </summary>
         public static bool IsLocked => Lockers.Any();
-
         /// <summary>
         /// Получить все текущие тачи.
         /// </summary>
@@ -61,14 +56,12 @@ namespace Extensions
             {
                 return new Touch[0];
             }
-
             return MakeFakeTouch(out var isZoomGesture)
                 ? isZoomGesture
                     ? new[] { ZoomGestureTouch1.Create(), ZoomGestureTouch2.Create() }
                     : new[] { LastFakeTouch.Create() }
                 : Input.touches;
         }
-
         /// <summary>
         /// Получить текущий тач.
         /// </summary>
@@ -83,23 +76,19 @@ namespace Extensions
                 touch = EmptyTouch;
                 return false;
             }
-
             if (Input.touchCount > touchNum)
             {
                 touch = Input.GetTouch(touchNum);
                 return true;
             }
-
             if (touchNum == 0 && MakeFakeTouch(out _))
             {
                 touch = LastFakeTouch.Create();
                 return true;
             }
-
             touch = EmptyTouch;
             return false;
         }
-
         /// <summary>
         /// Проверка на тач по элементу UI (не работает с UI в camera space).
         /// </summary>
@@ -110,17 +99,13 @@ namespace Extensions
             {
                 position = Input.touchCount > 0 ? Input.GetTouch(0).position : (Vector2)Input.mousePosition
             };
-
             Res.Clear();
             EventSystem.current.RaycastAll(ped, Res);
             int uiLayer = LayerMask.NameToLayer("UI");
             var result = Res.Exists(ray =>  ray.gameObject.layer == uiLayer);
-
             return result;
-
 //#if UNITY_EDITOR
 //            return EventSystem.current.IsPointerOverGameObject();
-
 //#else
 //			var ped = new PointerEventData(EventSystem.current)
 //			{
@@ -128,16 +113,13 @@ namespace Extensions
 //			};
 //			Res.Clear();
 //			EventSystem.current.RaycastAll(ped, Res);
-
 //			return Res.Count > 0;
 //#endif
         }
-
         private static bool MakeFakeTouch(out bool isZoomGesture)
         {
             isZoomGesture = false;
             if (Input.touchSupported) return false;
-
             if (Input.GetMouseButtonDown(0))
             {
                 LastFakeTouch.Phase = TouchPhase.Began;
@@ -164,13 +146,11 @@ namespace Extensions
             else if (Input.GetMouseButtonDown(1))
             {
                 isZoomGesture = true;
-
                 var p = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                 ZoomGestureTouch1.Phase = TouchPhase.Began;
                 ZoomGestureTouch1.DeltaPosition = Vector2.zero;
                 ZoomGestureTouch1.Position = p;
                 ZoomGestureTouch1.FingerId = 0;
-
                 ZoomGestureTouch2.Phase = TouchPhase.Began;
                 ZoomGestureTouch2.DeltaPosition = Vector2.zero;
                 ZoomGestureTouch2.Position = Mirror(p);
@@ -179,13 +159,11 @@ namespace Extensions
             else if (Input.GetMouseButtonUp(1))
             {
                 isZoomGesture = true;
-
                 var p = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                 ZoomGestureTouch1.Phase = TouchPhase.Ended;
                 ZoomGestureTouch1.DeltaPosition = p - ZoomGestureTouch1.Position;
                 ZoomGestureTouch1.Position = p;
                 ZoomGestureTouch1.FingerId = 0;
-
                 p = Mirror(p);
                 ZoomGestureTouch2.Phase = TouchPhase.Ended;
                 ZoomGestureTouch2.DeltaPosition = p - ZoomGestureTouch2.Position;
@@ -195,13 +173,11 @@ namespace Extensions
             else if (Input.GetMouseButton(1))
             {
                 isZoomGesture = true;
-
                 var p = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                 ZoomGestureTouch1.Phase = TouchPhase.Moved;
                 ZoomGestureTouch1.DeltaPosition = p - ZoomGestureTouch1.Position;
                 ZoomGestureTouch1.Position = p;
                 ZoomGestureTouch1.FingerId = 0;
-
                 p = Mirror(p);
                 ZoomGestureTouch2.Phase = TouchPhase.Moved;
                 ZoomGestureTouch2.DeltaPosition = p - ZoomGestureTouch2.Position;
@@ -212,10 +188,8 @@ namespace Extensions
             {
                 return false;
             }
-
             return true;
         }
-
         private static Vector2 Mirror(Vector2 pos)
         {
             var center = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
